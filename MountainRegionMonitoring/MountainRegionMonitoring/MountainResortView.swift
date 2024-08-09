0bf64cb0b8ee24f0da95781b9fffe26d2821f86f
Jason <sanchezjason@me.com>
2024-08-09 15:24:57 -0600

    swiftui/clmonitor/region_monitor/mtn_resort_region- locationManager

--- MountainRegionMonitoring/MountainRegionMonitoring/MountainResortView.swift
+++ MountainRegionMonitoring/MountainRegionMonitoring/MountainResortView.swift
@@ -12,7 +12,7 @@ struct MountainResortView: View {
     @State private var locationManager = LocationManager.shared
     @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
     
-    let artwork = ["beaverCreekResort": "🍸", "vailMountainResort": "🥃"]
+    let artwork = ["beaverCreekResort": "🍸", "Vail_Mountain_Resort": "🥃"]
     
     var body: some View {
         ZStack(alignment: .bottom) {
