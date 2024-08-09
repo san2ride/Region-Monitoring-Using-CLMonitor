1c2ab2d0c7b8f20c8b19b7a25d19cca0810b983c
Jason <sanchezjason@me.com>
2024-08-09 14:27:52 -0600

    swiftui/clmonitor/region_monitor/mtn_resort_region- location manager, MKCoordinateRegion

---
+++ MountainRegionMonitoring/MountainRegionMonitoring/MountainResortView.swift
@@ -0,0 +1,19 @@
+//
+//  MountainResortView.swift
+//  MountainRegionMonitoring
+//
+//  Created by Jason Sanchez on 8/9/24.
+//
+
+import SwiftUI
+import MapKit
+
+struct MountainResortView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MountainResortView()
+}
---
+++ MountainRegionMonitoring/MountainRegionMonitoring/Preview Content/MountainRegionMonitoringApp.swift
@@ -0,0 +1,17 @@
+//
+//  MountainRegionMonitoringApp.swift
+//  MountainRegionMonitoring
+//
+//  Created by Jason Sanchez on 8/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MountainRegionMonitoringApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
